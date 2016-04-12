18b25f5c8ecc6559eea30a64d9fa006c3e7659c1
taysontey <taysontey@gmail.com>
2016-04-12 18:15:04 -0300

    Entity Mapping(1)

---
+++ Projeto.Entity/Entities/Jogador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entity.Entities
+{
+    public class Jogador
+    {
+        public virtual int IdJogador { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual string Apelido { get; set; }
+        public virtual DateTime DataNascimento { get; set; }
+        public virtual string Posicao { get; set; }
+        public virtual int IdTime { get; set; }
+
+        #region Relacionamentos
+
+        public virtual Time Time { get; set; }
+
+        #endregion
+    }
+}
---
+++ Projeto.Entity/Entities/Time.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entity.Entities
+{
+    public class Time
+    {
+        public virtual int IdTime { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual DateTime DataFundacao { get; set; }
+
+        #region Relacionamentos
+
+        public virtual ICollection<Jogador> Jogadores { get; set; }
+
+        #endregion
+    }
+}
